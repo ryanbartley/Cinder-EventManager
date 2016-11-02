0a5f05b74238e8f412e9b8926a29e54b6c9f4875
Ryan Bartley <ryanbartley@gmail.com>
2016-11-02 11:38:25 -0400

    Added cmake project

---
+++ proj/cmake/Cinder-EventManagerConfig.cmake
@@ -0,0 +1,26 @@
+if( NOT TARGET Cinder-EventManger )
+  get_filename_component( CINDER_EVENT_BLOCK_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE )
+  get_filename_component( CINDER_EVENT_INCLUDE_PATH "${CINDER_EVENT_BLOCK_PATH}/src" ABSOLUTE )
+	get_filename_component( CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../.." ABSOLUTE )
+
+  add_library( Cinder-EventManager
+    ${CINDER_EVENT_INCLUDE_PATH}/EventManager.cpp 
+    ${CINDER_EVENT_INCLUDE_PATH}/EventManagerBase.cpp 
+  )
+
+  target_compile_options( Cinder-EventManager PUBLIC "-std=c++11" )
+
+  target_include_directories( Cinder-EventManager PUBLIC "${CINDER_EVENT_INCLUDE_PATH}" )
+
+	if( NOT TARGET cinder )
+		    include( "${CINDER_PATH}/proj/cmake/configure.cmake" )
+		    find_package( cinder REQUIRED PATHS
+		        "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
+		        "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}" )
+	endif()
+  target_link_libraries( Cinder-EventManager PRIVATE cinder )
+	
+endif()
+
+
+
